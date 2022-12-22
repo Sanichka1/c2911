a45fd1885e22baf687f5ac4b19e63007cea443e0
Anusina <iamlady2003@gmai.com>
2022-12-22 18:18:16 +0200

    initial commit

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-class Student:
+#class Student:
+   # def __init__(self):
+     #   self.heigth=170
+#nick=Student()
+#print(nick.heigth)
+
+class Rec:
+    def area(self,a , b):
+        return a * b
+rect=Rec()
+print(rect.area(4,5))
